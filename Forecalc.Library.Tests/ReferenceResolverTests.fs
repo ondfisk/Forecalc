d549ac7fce0595b7e66f1a5337a4503c9ee43721
Rasmus Nielsen <rasmus.nielsen@me.com>
2012-07-09 19:51:58 +0200

    Simplified test names

--- Forecalc.Library.Tests/ReferenceResolverTests.fs
+++ Forecalc.Library.Tests/ReferenceResolverTests.fs
@@ -31,70 +31,69 @@ type ReferenceResolverTests () =
         columnFromAlpha "AP" |> should equal 42
 
     [<Test>]
-    member this.``UnresolvedRef(A1) -> { Sheet = "Sheet1" ;  Cell = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false }}``() =
+    member this.``A1 -> { Sheet = "Sheet1" ;  Cell = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false }}``() =
         A1Cell("A1") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } }))
 
     [<Test>]
-    member this.``UnresolvedRef(E5) -> { Sheet = "Sheet1" ;  Cell = { Row = 2 ; RowAbs = false ; Col = 2 ; ColAbs = false }}``() =
+    member this.``E5 -> { Sheet = "Sheet1" ;  Cell = { Row = 2 ; RowAbs = false ; Col = 2 ; ColAbs = false }}``() =
         A1Cell("E5") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 2 ; RowAbs = false ; Col = 2 ; ColAbs = false } }))
 
     [<Test>]
-    member this.``UnresolvedRef($A1) -> { Sheet = "Sheet1" ;  Cell = { Row = -2 ; RowAbs = false ; Col = 1 ; ColAbs = true }}``() =
+    member this.``$A1 -> { Sheet = "Sheet1" ;  Cell = { Row = -2 ; RowAbs = false ; Col = 1 ; ColAbs = true }}``() =
         A1Cell("$A1") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = -2 ; RowAbs = false ; Col = 1 ; ColAbs = true } }))
         
     [<Test>]
-    member this.``UnresolvedRef(A$1) -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = true ; Col = -2 ; ColAbs = false }}``() =
+    member this.``A$1 -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = true ; Col = -2 ; ColAbs = false }}``() =
         A1Cell("A$1") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 1 ; RowAbs = true ; Col = -2 ; ColAbs = false } }))
 
     [<Test>]
-    member this.``UnresolvedRef($A$1) -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }}``() =
+    member this.``$A$1 -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }}``() =
         A1Cell("$A$1") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true } }))
 
     [<Test>]
-    member this.``UnresolvedRef(A1:B2) -> { Sheet = "Sheet1" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
+    member this.``A1:B2 -> { Sheet = "Sheet1" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
         A1Range("A1", "B2") |> resolveRef cell |> should equal (RangeRef({ Sheet = "Sheet1" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
         
     [<Test>]
-    member this.``UnresolvedRef(Sheet2!A1) -> { Sheet = "Sheet2" ;  Cell = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false }}``() =
+    member this.``Sheet2!A1 -> { Sheet = "Sheet2" ;  Cell = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false }}``() =
         A1SheetRef("Sheet2", "A1") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet2" ; Cell = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } }))
 
     [<Test>]
-    member this.``UnresolvedRef(Sheet2!A1:B2) -> { Sheet = "Sheet2" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
+    member this.``Sheet2!A1:B2 -> { Sheet = "Sheet2" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
         A1SheetRange("Sheet2", "A1", "B2") |> resolveRef cell |> should equal (RangeRef({ Sheet = "Sheet2" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
 
     [<Test>]
-    member this.``UnresolvedRef(R1C1) -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }}``() =
+    member this.``R1C1 -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }}``() =
         R1C1Cell("R1C1") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true } }))
 
     [<Test>]
-    member this.``UnresolvedRef(R[1]C1) -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = false ; Col = 1 ; ColAbs = true }}``() =
+    member this.``R[1]C1 -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = false ; Col = 1 ; ColAbs = true }}``() =
         R1C1Cell("R[1]C1") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 1 ; RowAbs = false ; Col = 1 ; ColAbs = true } }))
  
     [<Test>]
-    member this.``UnresolvedRef(R1C[-1]) -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = true ; Col = -1 ; ColAbs = false }}``() =
+    member this.``R1C[-1] -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = true ; Col = -1 ; ColAbs = false }}``() =
         R1C1Cell("R1C[-1]") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 1 ; RowAbs = true ; Col = -1 ; ColAbs = false } }))
          
     [<Test>]
-    member this.``UnresolvedRef(RC) -> { Sheet = "Sheet1" ;  Cell = { Row = 0 ; RowAbs = false ; Col = 0 ; ColAbs = false }}``() =
+    member this.``RC -> { Sheet = "Sheet1" ;  Cell = { Row = 0 ; RowAbs = false ; Col = 0 ; ColAbs = false }}``() =
         R1C1Cell("RC") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 0 ; RowAbs = false ; Col = 0 ; ColAbs = false } }))
 
     [<Test>]
-    member this.``UnresolvedRef(RC[-1]) -> { Sheet = "Sheet1" ;  Cell = { Row = 0 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
+    member this.``RC[-1] -> { Sheet = "Sheet1" ;  Cell = { Row = 0 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
         R1C1Cell("RC[-1]") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 0 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
         
     [<Test>]
-    member this.``UnresolvedRef(R[+1]C) -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = false ; Col = 0 ; ColAbs = false }}``() =
+    member this.``R[+1]C -> { Sheet = "Sheet1" ;  Cell = { Row = 1 ; RowAbs = false ; Col = 0 ; ColAbs = false }}``() =
         R1C1Cell("R[+1]C") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet1" ; Cell = { Row = 1 ; RowAbs = false ; Col = 0 ; ColAbs = false } }))
 
     [<Test>]
-    member this.``UnresolvedRef(R[-2]C[-2]:R[-1]C[-1]) -> { Sheet = "Sheet1" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
+    member this.``R[-2]C[-2]:R[-1]C[-1] -> { Sheet = "Sheet1" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
         R1C1Range("R[-2]C[-2]", "R[-1]C[-1]") |> resolveRef cell |> should equal (RangeRef({ Sheet = "Sheet1" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
         
     [<Test>]
-    member this.``UnresolvedRef(Sheet2!R1C1) -> { Sheet = "Sheet2" ; Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }}``() =
+    member this.``Sheet2!R1C1 -> { Sheet = "Sheet2" ; Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }}``() =
         R1C1SheetRef("Sheet2", "R1C1") |> resolveRef cell |> should equal (CellRef({ Sheet = "Sheet2" ; Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true } }))
 
     [<Test>]
-    member this.``UnresolvedRef(Sheet2!R[-2]C[-2]:R[-1]C[-1]) -> { Sheet = "Sheet2" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
+    member this.``Sheet2!R[-2]C[-2]:R[-1]C[-1] -> { Sheet = "Sheet2" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }}``() =
         R1C1SheetRange("Sheet2", "R[-2]C[-2]", "R[-1]C[-1]") |> resolveRef cell |> should equal (RangeRef({ Sheet = "Sheet2" ; TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
-
