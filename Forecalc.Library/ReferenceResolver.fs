2001e22ba7596768ce74e39ead15440392efd25c
Rasmus Nielsen <rasmus.nielsen@me.com>
2012-07-30 16:11:14 +0200

    Formatting

--- Forecalc.Library/ReferenceResolver.fs
+++ Forecalc.Library/ReferenceResolver.fs
@@ -52,7 +52,12 @@ module ReferenceResolver =
             
     let rec resolveRefs cell expr =
         match expr with
-            | Float(_) | Boolean(_) | String(_) | EscapedString(_) | Ref(_) | Error(_) -> expr
+            | Float(_) 
+            | Boolean(_) 
+            | String(_) 
+            | EscapedString(_) 
+            | Ref(_) 
+            | Error(_) -> expr
             | Negate(e) -> Negate(resolveRefs cell e)
             | Eq(e1, e2) -> Eq(resolveRefs cell e1, resolveRefs cell e2)
             | NotEq(e1, e2) -> NotEq(resolveRefs cell e1, resolveRefs cell e2)
