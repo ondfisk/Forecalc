229371eb32d0edd590e187a4e0939f5878da30f0
Rasmus Nielsen <rasmus.nielsen@me.com>
2012-07-30 15:12:34 +0200

    Begun eval function

---
+++ Forecalc.Library/Eval.fs
@@ -0,0 +1,14 @@
+﻿namespace Forecalc.Library
+
+open System
+open System.Globalization
+open System.Threading
+open Microsoft.FSharp.Text.Lexing
+open Ast
+open LexerSpecification
+open ParserSpecification
+
+module Eval =
+
+    let rec eval cell expr workbook =
+        Float(42.0) 
