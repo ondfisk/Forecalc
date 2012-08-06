1c716f0ad94c9808ee497fec5c5fae78232068e8
Rasmus Nielsen <rasmus.nielsen@me.com>
2012-08-06 10:40:22 +0200

    Array accessor for quadtree + [,] option
    Removed nasty null checks

--- Forecalc.Library.Tests/QuadTreeTests.fs
+++ Forecalc.Library.Tests/QuadTreeTests.fs
@@ -6,44 +6,69 @@ open FsUnit
 open Ast
 open Forecalc.Library
 
-let quadtree = QuadTree.create<int> 
-
 [<Test>]
 let ``QuadTree.get (-1, 0) should fail``() =
+    let quadtree = QuadTree.create<int> 
     (fun () -> quadtree |> QuadTree.get (-1, 0) |> ignore) |> should throw typeof<System.Exception>
  
 [<Test>]
 let ``QuadTree.get (0, -1) should fail``() =
+    let quadtree = QuadTree.create<int> 
     (fun () -> quadtree |> QuadTree.get (0, -1) |> ignore) |> should throw typeof<System.Exception>
  
 [<Test>]
 let ``QuadTree.get (65536, 0) should fail``() =
+    let quadtree = QuadTree.create<int> 
     (fun () -> quadtree |> QuadTree.get (65536, 0) |> ignore) |> should throw typeof<System.Exception>
  
 [<Test>]
 let ``QuadTree.get (0, 1048576) should fail``() =
+    let quadtree = QuadTree.create<int> 
     (fun () -> quadtree |> QuadTree.get (0, 1048576) |> ignore) |> should throw typeof<System.Exception>
 
 [<Test>]
 let ``QuadTree.set (-1, 0) 42 should fail``() =
-    (fun () -> quadtree |> QuadTree.set (-1, 0) 42 |> ignore) |> should throw typeof<System.Exception>
+    let quadtree = QuadTree.create<int> 
+    (fun () -> quadtree |> QuadTree.set (-1, 0) (Some 42) |> ignore) |> should throw typeof<System.Exception>
  
 [<Test>]
 let ``QuadTree.set (0, -1) 42 should fail``() =
-    (fun () -> quadtree |> QuadTree.set (0, -1) 42 |> ignore) |> should throw typeof<System.Exception>
+    let quadtree = QuadTree.create<int> 
+    (fun () -> quadtree |> QuadTree.set (0, -1) (Some 42) |> ignore) |> should throw typeof<System.Exception>
  
 [<Test>]
 let ``QuadTree.set (65536, 0) 42 should fail``() =
-    (fun () -> quadtree |> QuadTree.set (65536, 0) 42 |> ignore) |> should throw typeof<System.Exception>
+    let quadtree = QuadTree.create<int> 
+    (fun () -> quadtree |> QuadTree.set (65536, 0) (Some 42) |> ignore) |> should throw typeof<System.Exception>
  
 [<Test>]
 let ``QuadTree.set (0, 1048576) 42 should fail``() =
-    (fun () -> quadtree |> QuadTree.set (0, 1048576) 42 |> ignore) |> should throw typeof<System.Exception>
+    let quadtree = QuadTree.create<int> 
+    (fun () -> quadtree |> QuadTree.set (0, 1048576) (Some 42) |> ignore) |> should throw typeof<System.Exception>
+
+[<Test>]
+let ``QuadTree.mapInPlace (*) 2 should multiply elements by 2``() =
+    let quadtree = QuadTree.create<int> 
+    quadtree |> QuadTree.set (0, 0) (Some(21))
+    quadtree |> QuadTree.mapInPlace ((*) 2)
+    quadtree |> QuadTree.get (0, 0) |> should equal (Some 42)
+
+[<Test>]
+let ``QuadTree.iter should call all elements``() =
+    let quadtree = QuadTree.create<int> 
+    quadtree |> QuadTree.set (0, 0) (Some 9)
+    quadtree |> QuadTree.set (42, 42) (Some 10)
+    quadtree |> QuadTree.set (1000, 1000) (Some 11)
+    quadtree |> QuadTree.set (10000, 1000000) (Some 12)
+    let i = ref 0
+    quadtree |> QuadTree.iter (fun x -> i := !i + x)
+    !i |> should equal 42
 
 [<Test>]
 let ``QuadTree.toSeq returns sequence``() =
-    quadtree |> QuadTree.set (0, 0) 0
-    quadtree |> QuadTree.set (42, 42) 42
-    quadtree |> QuadTree.set (1000, 1000) 1000
-    quadtree |> QuadTree.set (10000, 1000000) 1000000
-    quadtree |> QuadTree.toSeq |> should equal (List.toSeq [ 0 ; 42 ; 1000 ; 1000000 ])
+    let quadtree = QuadTree.create<int> 
+    quadtree |> QuadTree.set (0, 0) (Some 9)
+    quadtree |> QuadTree.set (42, 42) (Some 10)
+    quadtree |> QuadTree.set (1000, 1000) (Some 11)
+    quadtree |> QuadTree.set (10000, 1000000) (Some 12)
+    quadtree |> QuadTree.toSeq |> should equal (List.toSeq [ 9 ; 10 ; 11 ; 12 ])
--- Forecalc.Library/QuadTree.fs
+++ Forecalc.Library/QuadTree.fs
@@ -19,84 +19,91 @@ module QuadTree =
             failwithf "r must be between 0 and %i" maxh
 
     type quadtree<'a> internal() =
-        let item = Array2D.zeroCreate<'a option [,] [,] [,]> w h
+        let array = Array2D.zeroCreate<'a option [,] option [,] option [,] option> w h
         with 
-            member internal this.Item 
-                with get() = item
+            member internal this.Array 
+                with get() = array
+            member this.Item
+                with get(c, r) =
+                        validate (c, r)
+                        let l3 = array.[(c >>> (3 * logw)) &&& mw, (r >>> (3 * logh)) &&& mh]
+                        match l3 with
+                            | None -> None
+                            | Some(value) ->
+                                let l2 = value.[(c >>> (2 * logw)) &&& mw, (r >>> (2 * logh)) &&& mh]
+                                match l2 with
+                                    | None -> None
+                                    | Some(value) ->
+                                        let l1 = value.[(c >>> logw) &&& mw, (r >>> logh) &&& mh]
+                                        match l1 with
+                                            | None -> None
+                                            | Some (value) -> value.[c &&& mw, r &&& mh]
+
+                and set(c, r) value =
+                        validate (c, r)
+                        let l3() = array.[(c >>> (3 * logw)) &&& mw, (r >>> (3 * logh)) &&& mh]
+                        if l3().IsNone then
+                            array.[(c >>> (3 * logw)) &&& mw, (r >>> (3 * logh)) &&& mh] <- Some(Array2D.zeroCreate<'a option [,] option [,] option> w h)
+                        let l2() = l3().Value.[(c >>> (2 * logw)) &&& mw, (r >>> (2 * logh)) &&& mh]
+                        if l2().IsNone then
+                            l3().Value.[(c >>> (2 * logw)) &&& mw, (r >>> (2 * logh)) &&& mh] <- Some(Array2D.zeroCreate<'a option [,] option> w h)
+                        let l1() = l2().Value.[(c >>> logw) &&& mw, (r >>> logh) &&& mh]
+                        if l1().IsNone then
+                            l2().Value.[(c >>> logw) &&& mw, (r >>> logh) &&& mh] <- Some(Array2D.zeroCreate<'a option> w h)
+                        l1().Value.[c &&& mw, r &&& mh] <- value
 
     let create<'a> = quadtree<'a>()
 
     let get (c, r) (quadtree : quadtree<'a>) =
-        validate (c, r)
-        let l3 = quadtree.Item.[(c >>> (3 * logw)) &&& mw, (r >>> (3 * logh)) &&& mh]
-        if l3 = null then 
-            None 
-        else
-            let l2 = l3.[(c >>> (2 * logw)) &&& mw, (r >>> (2 * logh)) &&& mh]
-            if l2 = null then
-                None
-            else
-                let l1 = l2.[(c >>> logw) &&& mw, (r >>> logh) &&& mh]
-                if l1 = null then
-                    None
-                else
-                    l1.[c &&& mw, r &&& mh]
+        quadtree.[c, r]
         
     let set (c, r) value (quadtree : quadtree<'a>) =
-        validate (c, r)
-        let l3() = quadtree.Item.[(c >>> (3 * logw)) &&& mw, (r >>> (3 * logh)) &&& mh]
-        if l3() = null then
-            quadtree.Item.[(c >>> (3 * logw)) &&& mw, (r >>> (3 * logh)) &&& mh] <- Array2D.zeroCreate<'a option [,] [,]> w h
-        let l2() = l3().[(c >>> (2 * logw)) &&& mw, (r >>> (2 * logh)) &&& mh]
-        if l2() = null then
-            l3().[(c >>> (2 * logw)) &&& mw, (r >>> (2 * logh)) &&& mh] <- Array2D.zeroCreate<'a option [,]> w h
-        let l1() = l2().[(c >>> logw) &&& mw, (r >>> logh) &&& mh]
-        if l1() = null then
-            l2().[(c >>> logw) &&& mw, (r >>> logh) &&& mh] <- Array2D.zeroCreate<'a option> w h
-        l1().[c &&& mw, r &&& mh] <- Some(value)
+        quadtree.[c, r] <- value
 
-    let iter f (quadtree : 'a option [,] [,] [,] [,]) =
-        quadtree |> Array2D.iter (fun l3 ->
-            if l3 <> null then
-                l3 |> Array2D.iter (fun l2 ->
-                    if l2 <> null then
-                        l2 |> Array2D.iter (fun l1 ->
-                            if l1 <> null then
-                                l1 |> Array2D.iter (fun l0 ->
+    let iter f (quadtree : quadtree<'a>) =
+        quadtree.Array |> Array2D.iter (fun l3 ->
+            if l3.IsSome then
+                l3.Value |> Array2D.iter (fun l2 ->
+                    if l2.IsSome then
+                        l2.Value |> Array2D.iter (fun l1 ->
+                            if l1.IsSome then
+                                l1.Value |> Array2D.iter (fun l0 ->
                                     match l0 with
                                         | Some(value) -> f value
                                         | None -> ()))))
 
     // skulle den hedde map eller apply eller??? 
     let mapInPlace f (quadtree : quadtree<'a>) =
-        quadtree.Item |> Array2D.iteri (fun c3 r3 l3 ->
-            if l3 <> null then
-                l3 |> Array2D.iteri (fun c2 r2 l2 ->
-                    if l2 <> null then
-                        l2 |> Array2D.iteri (fun c1 r1 l1 ->
-                            if l1 <> null then
-                                l1 |> Array2D.iteri (fun c0 r0 l0 ->
+        quadtree.Array |> Array2D.iteri (fun c3 r3 l3 ->
+            if l3.IsSome then
+                l3.Value |> Array2D.iteri (fun c2 r2 l2 ->
+                    if l2.IsSome then
+                        l2.Value |> Array2D.iteri (fun c1 r1 l1 ->
+                            if l1.IsSome then
+                                l1.Value |> Array2D.iteri (fun c0 r0 l0 ->
                                     match l0 with
-                                        | Some(value) -> quadtree.Item.[c3, r3].[c3, r2].[c1, r1].[c0, r0] <- Some(f value)
+                                        | Some(value) -> quadtree.Array.[c3, r3].Value.[c3, r2].Value.[c1, r1].Value.[c0, r0] <- Some(f value)
                                         | None -> ()))))
-
+    
+    // mangler en mapi, en iteri og en filter funktion.
+                                        
     let toSeq (quadtree : quadtree<'a>) =
         seq {
             for c in [ 0 .. mw ] do
                 for r in [ 0 .. mh ] do
-                    let l = quadtree.Item.[c, r]
-                    if l <> null then
+                    let l = quadtree.Array.[c, r]
+                    if l.IsSome then
                         for c in [ 0 .. mw ] do
                             for r in [ 0 .. mh ] do
-                                let l = l.[c, r]
-                                if l <> null then
+                                let l = l.Value.[c, r]
+                                if l.IsSome then
                                     for c in [ 0 .. mw ] do
                                         for r in [ 0 .. mh ] do
-                                            let l = l.[c, r]
-                                            if l <> null then
+                                            let l = l.Value.[c, r]
+                                            if l.IsSome then
                                                 for c in [ 0 .. mw ] do
                                                     for r in [ 0 .. mh ] do
-                                                        let l = l.[c, r]
+                                                        let l = l.Value.[c, r]
                                                         match l with
                                                             | Some(value) -> yield value
                                                             | None -> ()
