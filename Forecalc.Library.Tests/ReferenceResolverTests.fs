4c1305744242546af298bf628826219601ca0a2c
Rasmus Nielsen <rasmus.nielsen@me.com>
2012-08-08 15:53:31 +0200

    Option.None ->None

--- Forecalc.Library.Tests/ReferenceResolverTests.fs
+++ Forecalc.Library.Tests/ReferenceResolverTests.fs
@@ -39,39 +39,39 @@ let ``columnFromAlpha "XFD" -> 16384``() =
 
 [<Test>]
 let ``A1 -> { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false }``() =
-    A1Cell("A1") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false }))
+    A1Cell("A1") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false }))
 
 [<Test>]
 let ``E5 -> { Row = 2 ; RowAbs = false ; Col = 2 ; ColAbs = false }``() =
-    A1Cell("E5") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = false ; Col = 2 ; ColAbs = false }))
+    A1Cell("E5") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 2 ; RowAbs = false ; Col = 2 ; ColAbs = false }))
 
 [<Test>]
 let ``$A1 -> { Row = -2 ; RowAbs = false ; Col = 1 ; ColAbs = true }``() =
-    A1Cell("$A1") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = -2 ; RowAbs = false ; Col = 1 ; ColAbs = true }))
+    A1Cell("$A1") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = -2 ; RowAbs = false ; Col = 1 ; ColAbs = true }))
         
 [<Test>]
 let ``A$1 -> { Row = 1 ; RowAbs = true ; Col = -2 ; ColAbs = false }``() =
-    A1Cell("A$1") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = -2 ; ColAbs = false }))
+    A1Cell("A$1") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = -2 ; ColAbs = false }))
 
 [<Test>]
 let ``$A$1 -> { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }``() =
-    A1Cell("$A$1") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))
+    A1Cell("$A$1") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))
 
 [<Test>]
 let ``A1:B2 -> { TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }``() =
-    A1Range("A1", "B2") |> resolveRef cell |> should equal (Range({ Sheet = Option.None ; TopLeft = { Sheet = Option.None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = Option.None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
+    A1Range("A1", "B2") |> resolveRef cell |> should equal (Range({ Sheet = None ; TopLeft = { Sheet = None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
         
 [<Test>]
 let ``B2:A1 -> { TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }``() =
-    A1Range("B2", "A1") |> resolveRef cell |> should equal (Range({ Sheet = Option.None ; TopLeft = { Sheet = Option.None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = Option.None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
+    A1Range("B2", "A1") |> resolveRef cell |> should equal (Range({ Sheet = None ; TopLeft = { Sheet = None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
       
 [<Test>]
 let ``A2:B1 -> { TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }``() =
-    A1Range("A2", "B1") |> resolveRef cell |> should equal (Range({ Sheet = Option.None ; TopLeft = { Sheet = Option.None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = Option.None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
+    A1Range("A2", "B1") |> resolveRef cell |> should equal (Range({ Sheet = None ; TopLeft = { Sheet = None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
 
 [<Test>]
 let ``B1:A2 -> { TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false }``() =
-    A1Range("B1", "A2") |> resolveRef cell |> should equal (Range({ Sheet = Option.None ; TopLeft = { Sheet = Option.None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = Option.None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
+    A1Range("B1", "A2") |> resolveRef cell |> should equal (Range({ Sheet = None ; TopLeft = { Sheet = None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
               
 [<Test>]
 let ``Sheet2!A1 -> { Sheet = "Sheet2" ;  Cell = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } }``() =
@@ -91,35 +91,35 @@ let ``A1Cell(R1C1) -> fail``() =
 
 [<Test>]
 let ``R1C1 -> { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }``() =
-    R1C1Cell("R1C1") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))
+    R1C1Cell("R1C1") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))
 
 [<Test>]
 let ``R[1]C1 -> { Row = 1 ; RowAbs = false ; Col = 1 ; ColAbs = true }``() =
-    R1C1Cell("R[1]C1") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = false ; Col = 1 ; ColAbs = true }))
+    R1C1Cell("R[1]C1") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 1 ; RowAbs = false ; Col = 1 ; ColAbs = true }))
  
 [<Test>]
 let ``R1C[-1] -> { Row = 1 ; RowAbs = true ; Col = -1 ; ColAbs = false }``() =
-    R1C1Cell("R1C[-1]") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = -1 ; ColAbs = false }))
+    R1C1Cell("R1C[-1]") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = -1 ; ColAbs = false }))
          
 [<Test>]
 let ``RC -> { Row = 0 ; RowAbs = false ; Col = 0 ; ColAbs = false }``() =
-    R1C1Cell("RC") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 0 ; RowAbs = false ; Col = 0 ; ColAbs = false }))
+    R1C1Cell("RC") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 0 ; RowAbs = false ; Col = 0 ; ColAbs = false }))
 
 [<Test>]
 let ``RC[-1] -> { Row = 0 ; RowAbs = false ; Col = -1 ; ColAbs = false }``() =
-    R1C1Cell("RC[-1]") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 0 ; RowAbs = false ; Col = -1 ; ColAbs = false }))
+    R1C1Cell("RC[-1]") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 0 ; RowAbs = false ; Col = -1 ; ColAbs = false }))
         
 [<Test>]
 let ``R[+1]C -> { Row = 1 ; RowAbs = false ; Col = 0 ; ColAbs = false }``() =
-    R1C1Cell("R[+1]C") |> resolveRef cell |> should equal (Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = false ; Col = 0 ; ColAbs = false }))
+    R1C1Cell("R[+1]C") |> resolveRef cell |> should equal (Cell({ Sheet = None ; Row = 1 ; RowAbs = false ; Col = 0 ; ColAbs = false }))
 
 [<Test>]
 let ``R[-2]C[-2]:R[-1]C[-1] -> { TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }``() =
-    R1C1Range("R[-2]C[-2]", "R[-1]C[-1]") |> resolveRef cell |> should equal (Range({ Sheet = Option.None ;  TopLeft = { Sheet = Option.None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = Option.None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
+    R1C1Range("R[-2]C[-2]", "R[-1]C[-1]") |> resolveRef cell |> should equal (Range({ Sheet = None ;  TopLeft = { Sheet = None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
         
 [<Test>]
 let ``R[-1]C[-1]:R[-2]C[-2] -> { TopLeft = { Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }``() =
-    R1C1Range("R[-1]C[-1]", "R[-2]C[-2]") |> resolveRef cell |> should equal (Range({ Sheet = Option.None ;  TopLeft = { Sheet = Option.None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = Option.None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
+    R1C1Range("R[-1]C[-1]", "R[-2]C[-2]") |> resolveRef cell |> should equal (Range({ Sheet = None ;  TopLeft = { Sheet = None ; Row = -2 ; RowAbs = false ; Col = -2 ; ColAbs = false } ; BottomRight = { Sheet = None ; Row = -1 ; RowAbs = false ; Col = -1 ; ColAbs = false } }))
         
 [<Test>]
 let ``Sheet2!R1C1 -> { Sheet = "Sheet2" ; Cell = { Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true } }``() =
@@ -155,67 +155,67 @@ let ``"42" -> EscapedString "42"``() =
 
 [<Test>]
 let ``-R1C1 -> Negate(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })))``() =
-    Negate(UnresolvedRef(R1C1Cell("R1C1"))) |> resolveRefs cell |> should equal (Negate(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))))
+    Negate(UnresolvedRef(R1C1Cell("R1C1"))) |> resolveRefs cell |> should equal (Negate(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1=R2C2 -> Eq(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Eq(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Eq(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Eq(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Eq(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1<>R2C2 -> NotEq(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    NotEq(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (NotEq(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    NotEq(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (NotEq(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1<R2C2 -> Lt(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Lt(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Lt(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Lt(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Lt(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1<=R2C2 -> Lte(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Lte(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Lte(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Lte(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Lte(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1>R2C2 -> Gt(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Gt(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Gt(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Gt(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Gt(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1>=R2C2 -> Gte(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Gte(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Gte(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Gte(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Gte(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1&R2C2 -> Concat(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Concat(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Concat(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Concat(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Concat(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1+R2C2 -> Add(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Add(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Add(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Add(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Add(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1-R2C2 -> Sub(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Sub(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Sub(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Sub(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Sub(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1*R2C2 -> Mul(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Mul(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Mul(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Mul(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Mul(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1/R2C2 -> Div(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Div(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Div(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Div(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Div(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``R1C1^R2C2 -> Pow(Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true })))``() =
-    Pow(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Pow(Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = Option.None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
+    Pow(UnresolvedRef(R1C1Cell("R1C1")), UnresolvedRef(R1C1Cell("R2C2"))) |> resolveRefs cell |> should equal (Pow(Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })), Ref(Cell({ Sheet = None ; Row = 2 ; RowAbs = true ; Col = 2 ; ColAbs = true }))))
 
 [<Test>]
 let ``Ref -> Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })``() =
-    Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })) |> resolveRefs cell |> should equal (Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })))
+    Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })) |> resolveRefs cell |> should equal (Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })))
 
 [<Test>]
 let ``R1C1 -> Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })``() =
-    UnresolvedRef(R1C1Cell("R1C1")) |> resolveRefs cell |> should equal (Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })))
+    UnresolvedRef(R1C1Cell("R1C1")) |> resolveRefs cell |> should equal (Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true })))
 
 [<Test>]
 let ``Sum(42.0, R1C1) -> Fun("Sum", [Float 42.0 ; Ref(Cell({ Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))])``() =
-    Fun("Sum", [Float 42.0 ; UnresolvedRef(R1C1Cell("R1C1"))]) |> resolveRefs cell |> should equal (Fun("Sum", [Float 42.0 ; Ref(Cell({ Sheet = Option.None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))]))
+    Fun("Sum", [Float 42.0 ; UnresolvedRef(R1C1Cell("R1C1"))]) |> resolveRefs cell |> should equal (Fun("Sum", [Float 42.0 ; Ref(Cell({ Sheet = None ; Row = 1 ; RowAbs = true ; Col = 1 ; ColAbs = true }))]))
 
 [<Test>]
 let ``Error "42" -> Error "42"``() =
