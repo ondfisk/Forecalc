51e4db730a641f4010b1d423fb95b4462127bb65
Rasmus Nielsen <rasmus.nielsen@me.com>
2012-10-03 14:39:30 +0200

    Tangent added and tested

---
+++ Forecalc.Library.Math.Tests/TrigonometryTangentTests.fs
@@ -0,0 +1,76 @@
+﻿module TrigonometryTangentTests
+
+open System.Collections.Generic
+open NUnit.Framework
+open FsUnit
+open Forecalc.Library
+open Forecalc.Library.Ast
+open Forecalc.Library.Eval
+
+[<Test>]
+let ``TAN(0.0) -> FloatValue(0.0)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [ Float 0.0 ])
+    eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) |> should equal (FloatValue 0.0)
+
+[<Test>]
+let ``TAN(3.141593) -> FloatValue(0.0)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [ Float 3.141593 ])
+    match eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) with
+        | FloatValue(v) -> v |> should (equalWithin 0.001) 0.0
+        | _ -> failwith "Fail"
+
+[<Test>]
+let ``TAN(true) -> FloatValue(1.557407725)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [ Boolean true ])
+    match eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) with
+        | FloatValue(v) -> v |> should (equalWithin 0.001) 1.557407725
+        | _ -> failwith "Fail"
+    
+[<Test>]
+let ``TAN(false) -> FloatValue(0.0)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [ Boolean false ])
+    eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) |> should equal (FloatValue 0.0)
+
+[<Test>]
+let ``TAN(Blank) -> FloatValue(0.0)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [ Blank ])
+    eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) |> should equal (FloatValue 0.0)
+
+[<Test>]
+let ``TAN("42") -> ErrorValue(Value)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [ String "42" ])
+    eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) |> should equal (ErrorValue Value)
+    
+[<Test>]
+let ``TAN([]) -> ErrorValue(Parse)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [])
+    eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) |> should equal (ErrorValue Parse)
+    
+[<Test>]
+let ``TAN([ 0 ; 0 ]) -> ErrorValue(Parse)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [ Float 0.0 ; Float 0.0 ])
+    eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) |> should equal (ErrorValue Parse)
+ 
+[<Test>]
+let ``TAN(DivZero) -> ErrorValue(DivZero)``() =
+    let workbook = Map.ofList [ "Sheet1", (QT4.create<CellContent>()) ]
+    let cell = { Sheet = "Sheet1" ; Row = 1 ; Col = 1 }
+    let expr = Fun("TAN", [ Error DivZero ])
+    eval cell expr workbook (HashSet<AbsCell>()) (HashSet<AbsCell>()) |> should equal (ErrorValue DivZero)
+ 
--- Forecalc.Library.Math/Trigonometry.fs
+++ Forecalc.Library.Math/Trigonometry.fs
@@ -7,6 +7,19 @@ open System.ComponentModel.Composition.Hosting
 open Ast
 open Eval
 
+module Trigonometry =
+    let apply func cell list workbook dirty computing =
+        match list with
+            | [ e ] -> 
+                match eval cell e workbook dirty computing with
+                    | FloatValue v -> FloatValue (func v)
+                    | BooleanValue v -> FloatValue (func (toFloat v))
+                    | NullValue -> FloatValue (func 0.0)
+                    | ValueList _
+                    | StringValue _ -> ErrorValue Value
+                    | ErrorValue v -> ErrorValue v   
+            | _ -> ErrorValue Parse
+
 [<Export(typeof<ISheetFunction>)>]
 type Pi() = 
     interface ISheetFunction with
@@ -22,16 +35,7 @@ type Degrees() =
     interface ISheetFunction with
         member this.Name = "DEGREES"
         member this.Apply list cell workbook dirty computing =
-            match list with
-                | [ e ] -> 
-                    match eval cell e workbook dirty computing with
-                        | FloatValue v -> FloatValue (degrees v)
-                        | BooleanValue v -> FloatValue (degrees (toFloat v))
-                        | NullValue -> FloatValue (degrees 0.0)
-                        | ValueList _
-                        | StringValue _ -> ErrorValue Value
-                        | ErrorValue v -> ErrorValue v 
-                | _ -> ErrorValue Parse
+            Trigonometry.apply degrees cell list workbook dirty computing
 
 [<Export(typeof<ISheetFunction>)>]
 type Radians() = 
@@ -39,45 +43,25 @@ type Radians() =
     interface ISheetFunction with
         member this.Name = "RADIANS"
         member this.Apply list cell workbook dirty computing =
-            match list with
-                | [ e ] -> 
-                    match eval cell e workbook dirty computing with
-                        | FloatValue v -> FloatValue (radians v)
-                        | BooleanValue v -> FloatValue (radians (toFloat v))
-                        | NullValue -> FloatValue (radians 0.0)
-                        | ValueList _
-                        | StringValue _ -> ErrorValue Value
-                        | ErrorValue v -> ErrorValue v 
-                | _ -> ErrorValue Parse
+            Trigonometry.apply radians cell list workbook dirty computing
 
 [<Export(typeof<ISheetFunction>)>]
 type Cosine() = 
     interface ISheetFunction with
         member this.Name = "COS"
         member this.Apply list cell workbook dirty computing =
-            match list with
-                | [ e ] -> 
-                    match eval cell e workbook dirty computing with
-                        | FloatValue v -> FloatValue (Math.Cos v)
-                        | BooleanValue v -> FloatValue (Math.Cos (toFloat v))
-                        | NullValue -> FloatValue (Math.Cos 0.0)
-                        | ValueList _
-                        | StringValue _ -> ErrorValue Value
-                        | ErrorValue v -> ErrorValue v 
-                | _ -> ErrorValue Parse
+            Trigonometry.apply Math.Cos cell list workbook dirty computing
 
 [<Export(typeof<ISheetFunction>)>]
 type Sine() = 
     interface ISheetFunction with
         member this.Name = "SIN"
         member this.Apply list cell workbook dirty computing =
-            match list with
-                | [ e ] -> 
-                    match eval cell e workbook dirty computing with
-                        | FloatValue v -> FloatValue (Math.Sin v)
-                        | BooleanValue v -> FloatValue (Math.Sin (toFloat v))
-                        | NullValue -> FloatValue (Math.Sin 0.0)
-                        | ValueList _
-                        | StringValue _ -> ErrorValue Value
-                        | ErrorValue v -> ErrorValue v 
-                | _ -> ErrorValue Parse
+            Trigonometry.apply Math.Sin cell list workbook dirty computing
+
+[<Export(typeof<ISheetFunction>)>]
+type Tanget() = 
+    interface ISheetFunction with
+        member this.Name = "TAN"
+        member this.Apply list cell workbook dirty computing =
+            Trigonometry.apply Math.Tan cell list workbook dirty computing
